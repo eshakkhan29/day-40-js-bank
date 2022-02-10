110d95001163fcc7d47638b51de6922eedb5423f
eshakkhan29 <eshakkhan29@gmail.com>
2022-02-10 09:53:59 +0600

    first commit

---
+++ js/loogin.js
@@ -0,0 +1,13 @@
+// loog in js 
+document.getElementById('loogin-button').addEventListener('click', function(){
+    const accountInput = document.getElementById('account-input');
+    const accountValue = accountInput.value;
+    const passwordInput = document.getElementById('input-password');
+    const passwordValue = passwordInput.value;
+    if (accountValue == 'Account' && passwordValue == 'password') {
+        window.location.href = 'banking.html';
+    }
+    else{
+        alert('Input Account = Account, and password = password')
+    }
+});
